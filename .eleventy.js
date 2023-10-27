e9f533d4e055ebf15862014a51be3cb63bae6b2c
asing44 <adamwarsing@gmail.com>
2023-10-26 20:03:48 -0400

    Template creation Starting to create templates

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
           files: './dist/css/**/*.css'
       });
+
+    eleventyConfig.addPassthroughCopy("./src/assets");
   
     return {
       dir: {
