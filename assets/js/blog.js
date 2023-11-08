c1a5e941db630f6c227bbd6ba2f6d3c7b646dca7
asing44 <asing44@users.noreply.github.com>
2023-11-08 04:36:32 +0000

    deploy: 34d7883b4c4f164a43cb319fbe2735207b5c6359

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,24 +1,3 @@
 // ** ----------------------------------------
 // ** BLOG PAGE
 // ** ----------------------------------------
-
-// ** PIN MAIN ARTICLE
-const mainArticleScroll = gsap.timeline({
-    scrollTrigger: {
-        scrub: true,
-        trigger: ".articles-container",
-        pin: ".main-article",
-        start: "top 10%",
-        end: "bottom 75%",
-    }
-});
-
-const mainArticleScrollReverse = gsap.timeline({
-    scrollTrigger: {
-        scrub: true,
-        trigger: ".articles-container-reverse",
-        pin: ".main-article-reverse",
-        start: "top 10%",
-        end: "bottom 75%",
-    }
-});
