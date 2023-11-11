bf8198c3cdf6573c2302a53faea605172e7cc51f
asing44 <adamwarsing@gmail.com>
2023-11-11 02:53:17 -0500

    changing blog layout
    updating the blog page

--- dist/assets/js/blog.js
+++ dist/assets/js/blog.js
@@ -1,3 +1,17 @@
 // * SET CATEGORY COUNT
 
 $('.category-count').text($('.article-container').length);
+
+// ** ----------------------------------------
+// ** FOCUS SELCTION
+// ** ----------------------------------------
+
+// ** ARTICLE SNIPPETS
+
+$(".__article-snippet").hover(function() { // Mouse over
+    $(this).siblings().stop().fadeTo(300, 0.6);
+    $(this).parent().siblings().stop().fadeTo(300, 0.3); 
+  }, function() { // Mouse out
+    $(this).siblings().stop().fadeTo(300, 1);
+    $(this).parent().siblings().stop().fadeTo(300, 1);
+});
