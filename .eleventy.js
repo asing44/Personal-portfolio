32b8e7b656c65678a7bfb45204216cd5c59e8522
asing44 <109570966+asing44@users.noreply.github.com>
2023-11-28 12:18:02 -0500

    Changed dev live server

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
 module.exports = function(eleventyConfig) {
-    // Watch CSS files for changes
-    eleventyConfig.setBrowserSyncConfig({
-          files: './dist/**'
-    });
     // Copy files to dist
     eleventyConfig.addPassthroughCopy("./src/assets");
+    // Watch changes in scss files
+    eleventyConfig.setWatchThrottleWaitTime(100); 
+    eleventyConfig.addWatchTarget('./src/static/scss');
     // HTML Base plugin
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   
